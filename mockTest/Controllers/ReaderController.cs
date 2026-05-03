7b9a1eea94cdf94109b0a650b331509434ac14e7
Viktoriia <savchukv2006@gmail.com>
2026-05-03 17:33:51 +0200

    Add content to CustomerController

---
+++ mockTest/Controllers/ReaderController.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace mockTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReaderController : ControllerBase
+    {
+        private readonly IDbService _dbService;
+
+        public ReaderController(IDbService dbService)
+        {
+            _dbService = dbService;
+        }
+
+        [Route("{id}/loans")]
+        [HttpGet]
+        public async Task<IActionResult> GetById(int id)
+        {
+            try
+            {
+                var result = await _dbService.GetReadersLoansAsync(id);
+                return Ok(result);
+            }
+            catch (NotFoundxception e)
+            {
+                return NotFound(e.message);
+            }
+        }
+
+        [Route("{id}/loans")]
+        [HttpPost]
+        public async Task<IActionResult> Post([FromRoute] int id, [FromBody] CreateLoansWithBooksDto dto)
+        {
+            if (!dto.Books.Any())
+            {
+                return BadRequest("At least one book is required");
+            }
+
+            try
+            {
+                await _dbService.CreateLoansWithBooksAsync(id, dto);
+                return Created($"api/readers/{id}/loans", dto);
+            }
+            catch (NotFoundException e)
+            {
+                return NotFound(e.message);
+            }
+        }
+    }
+}
