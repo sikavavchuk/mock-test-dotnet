60755da574eaa3122d3186ba48c631498b0c7b34
Viktoriia <savchukv2006@gmail.com>
2026-05-03 19:56:44 +0200

    Add content to program.cs

---
+++ mockTest/Program.cs
@@ -0,0 +1,25 @@
+using mockTest.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddScoped<IDbService, DbService>();
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwaggerUI(opt =>
+    {
+        opt.SwaggerEndpoint("/openapi/v1.json", "v1");
+    });
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
